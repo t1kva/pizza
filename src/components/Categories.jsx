1ee0c5525d9b04e594f459180393ca66144fc074
tikva <dattebayo231@gmail.com>
2024-01-19 12:08:38 +0300

    first

---
+++ src/components/Categories.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Categories = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            onClick={() => onClickCategory(i)}
+            className={activeIndex === i ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
