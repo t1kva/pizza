4ef81c065a1523570115da9826dc415ac9914fef
tikva <dattebayo231@gmail.com>
2024-01-21 15:45:31 +0300

    adaptive design + page cart

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="cart" element={<Cart />}/>
@@ -25,7 +24,6 @@ function App() {
           </Routes>
         </div>
       </div>
-    </div>
   );
 }
 
